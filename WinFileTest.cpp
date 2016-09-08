ac89f229d0788c015b549b79cbc5027517e79067
Bartłomiej Filipek <joebaf@gmail.com>
2016-09-08 08:14:36 +0200

    initial commit

---
+++ WinFileTest.cpp
@@ -0,0 +1,15 @@
+// WinFileTest.cpp
+// entry code for tests with accessing files on Windows: stdio, iostream, CreateFile, memory mapped
+// Bartlomiej Filipek, 2016, bfilipek.com
+
+#include "WINEXCLUDE.H"
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(int argc, LPTSTR argv[])
+{
+	printf("hello world!");
+
+	return 0;
+}
